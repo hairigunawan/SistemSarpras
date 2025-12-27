f2dfd1a2b665f9cb2fad91ed8fdfd1d0a04073bb
Hairi gunawan <gunawanhairi641@gmail.com>
2025-12-28 00:55:10 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/calender.js', 'resources/js/peminjaman.js', 'resources/js/hidenPassword.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/calender.js', 'resources/js/peminjaman.js', 'resources/js/hidenPassword.js', 'resources/js/notif.js', 'resources/js/tolak.js'],
             refresh: true,
         }),
     ],
